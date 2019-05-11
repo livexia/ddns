263a4d6a8f08f02408686f8df0becb2917cac188
livexia <istillalive@live.cn>
2019-05-11 14:10:29 +0800

    change how to get old IP

--- ddns.sh
+++ ddns.sh
@@ -4,7 +4,7 @@ then
     echo "Reading hostname,key and ip from config file: "$2
     HE_HOSTNAME=$(sed -n '1p' $2)
     HE_KEY=$(sed -n '2p' $2)
-    OLD_IP=$(nslookup -query=AAAA $HE_HOSTNAME | grep $HE_HOSTNAME | awk '{print $NF}')
+    OLD_IP=$(nslookup -query=AAAA $HE_HOSTNAME | grep -i addr | grep -v \# | awk '{print $NF}')
     IP=$(ifconfig | grep inet6 | grep -v fe | grep -v ::1 | awk '{print $2}')
     echo $OLD_IP 
     echo $IP
