9cfc9c4d9cd0710b3817b8fcd438bd908c177692
livexia <istillalive@live.cn>
2019-05-12 22:51:27 +0800

    add date

--- ddns.sh
+++ ddns.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+echo "-----------------------------------------------------------------------"
 if [ $# -eq 2 ]
 then
+    echo $(/bin/date)
     echo "Reading hostname,key and ip from config file: "$2
     HE_HOSTNAME=$(sed -n '1p' $2)
     HE_KEY=$(sed -n '2p' $2)
     OLD_IP=$(nslookup -query=AAAA $HE_HOSTNAME ns1.he.net | grep -i addr | grep -v \# | awk '{print $NF}')
     IP=$(ifconfig | grep inet6 | grep -v fe | grep -v ::1 | awk '{print $2}')
-    echo $OLD_IP 
-    echo $IP
+    echo "OLD IP:$OLD_IP"
+    echo "IP:$IP"
     if [ "$IP" = "$OLD_IP" ]
     then
         echo "IP is same as before, do nothing"
